df0a342f2e1ebb2f08d5a5d9facdff5f9d43ee63
Robert <robert.kabat.tcz@gmail.com>
2022-11-08 14:02:13 +0000

    cleaned up after git fail

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,6 @@ import { createApp } from 'vue'
 import App from './components/App.vue'
 
 const Vue = createApp(App).mount("#app")
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
